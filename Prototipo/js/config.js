2e4113719e9c6a439341f56df59ae3fd5c3a99b8
Jesús Alexander Herrera Acevedo <152633538+ExplosiveSmile05@users.noreply.github.com>
2025-05-04 03:23:52 +0000

    esperemos tengo fe

--- Prototipo/js/config.js
+++ Prototipo/js/config.js
@@ -1,4 +1,4 @@
 // config.js
-const apiUrl = 'https://ominous-fortnight-q77476q66q66fx46g-5000.app.github.dev/';
+const apiUrl = 'https://ominous-fortnight-q77476q66q66fx46g-5000.app.github.dev';
 
 export default apiUrl;
--- WebServices/WEBservices.py
+++ WebServices/WEBservices.py
@@ -527,4 +527,5 @@ def borrar_responde():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
